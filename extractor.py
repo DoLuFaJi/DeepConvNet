873a82edfa1ef11b44d2930945af9df4f9267d43
Lucas Marie <lucas.marie@insa-lyon.fr>
2018-10-27 09:58:01 +0200

    Run multiple times over training dataset

--- extractor.py
+++ extractor.py
@@ -23,28 +23,28 @@ classes = ('NOT FACE', 'FACE')
 net = Net()
 
 criterion = nn.CrossEntropyLoss()
+# lr is learning rate
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-# here used to go through dataset twice, but eof, must reset dataset
-running_loss = 0.0
-epoch = 0
-for i, data in enumerate(trainloader, 0):
-    # get the inputs
-    inputs, labels = data['image'], data['is_face']
-    # zero the parameter gradients
-    optimizer.zero_grad()
-    # forward + backward + optimize
-    outputs = net(inputs.float())
-    loss = criterion(outputs, labels)
-    loss.backward()
-    optimizer.step()
+for epoch in range(2):
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs
+        inputs, labels = data['image'], data['is_face']
+        # zero the parameter gradients
+        optimizer.zero_grad()
+        # forward + backward + optimize
+        outputs = net(inputs.float())
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
 
-    # print statistics
-    running_loss += loss.item()
-    if i % 2000 == 1999:    # print every 2000 mini-batches
-        print('[%d, %5d] loss: %.3f' %
-              (epoch + 1, i + 1, running_loss / 2000))
-        running_loss = 0.0
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
 
 print('Finished Training')
 
